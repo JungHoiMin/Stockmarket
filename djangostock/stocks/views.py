726ebee993b470bd0034390e0c62b0dc45327270
JungHoiMin <wony1102@naver.com>
2022-08-29 14:46:49 +0900

    주식 api 검색 기능 추가

--- djangostock/stocks/views.py
+++ djangostock/stocks/views.py
@@ -4,9 +4,14 @@ import json
 
 
 def home(request):
-    stock_api = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_6ce1d6df0d3643219faa325f0d61129a")
-    stock = json.loads(stock_api.content)
+    try:
+        ticker = request.GET['ticker']
+        stock_api = requests.get(
+            f"https://cloud.iexapis.com/stable/stock/{ticker}/quote?token=pk_6ce1d6df0d3643219faa325f0d61129a")
+        stock = json.loads(stock_api.content)
+    except Exception as e:
+        stock = ""
 
-    content = {'stock':stock}
+    content = {'stock': stock}
 
     return render(request, 'stocks/home.html', content)
