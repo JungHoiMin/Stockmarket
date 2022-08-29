e7a3aa81dd6f2787cd711685e9daecf374c38cfa
JungHoiMin <wony1102@naver.com>
2022-08-29 14:35:37 +0900

    IEX Cloud API 연결 및 임시 데이터 출력

--- djangostock/stocks/views.py
+++ djangostock/stocks/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+import requests
+import json
 
 
 def home(request):
-    return render(request, 'stocks/home.html')
+    stock_api = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_6ce1d6df0d3643219faa325f0d61129a")
+    stock = json.loads(stock_api.content)
+
+    content = {'stock':stock}
+
+    return render(request, 'stocks/home.html', content)
